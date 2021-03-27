f641bb883d8b109f7110a889bede3ea22c7613db
AzizbekJumanazarov <fais_le@mail.ru>
2021-03-28 00:24:16 +0500

    UI forms and make controller for Timetable

---
+++ resources/views/timetables/_form.blade.php
@@ -0,0 +1,30 @@
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title"><i class="fa fa-filter"></i> {{ 'Filter' }}</h3>
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                <i class="fas fa-minus"></i>
+            </button>
+        </div>
+    </div>
+    <div class="card-body" style="display: block;">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-4">
+                    <input type="text" class="form-control" placeholder="search">
+                </div>
+                <div class="col-2">
+                    <input type="select" class="form-control" placeholder="Teacher">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Group">
+                </div>
+                <div class="col-4">
+                    <input type="text" class="form-control" placeholder=".col-5">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.card-body -->
+</div>
---
+++ resources/views/timetables/index.blade.php
@@ -0,0 +1,30 @@
+@extends('adminlte::page')
+
+@section('title', 'Timetables')
+
+@section('content')
+    <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">{{ 'Timetables' }}</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body" style="display: block;">
+                <div class="card-body">
+                    @include('timetables._form')
+                    @include('timetables._grid')
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+@stop

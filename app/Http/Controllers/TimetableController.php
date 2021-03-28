d5cb231d87c2dd1943a76ec5394d311d65767033
AzizbekJumanazarov <fais_le@mail.ru>
2021-03-28 08:50:01 +0500

    Created TimetableController

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -2,27 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Timetable as TimetableRequest;
+use App\Services\Timetable as TimetableService;
 
+/**
+ * Class TimetableController
+ * @package App\Http\Controllers
+ */
 class TimetableController extends Controller
 {
+    /**
+     * @var TimetableService
+     */
+    protected $service;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(TimetableService $service)
     {
         $this->middleware('auth');
+        $this->service = $service;
     }
 
     /**
      * Show the application dashboard.
      *
+     * @param TimetableRequest $request
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(TimetableRequest $request)
     {
-        return view('timetables.index');
+        return view('timetables.index', [
+            'list' => $this->service->getByFilter($request->validated()),
+            'options' => $this->service->options(),
+        ]);
     }
 }
